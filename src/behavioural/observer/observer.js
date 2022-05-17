ca08f32cff712f4d472fbd456b1e1dc0e6f14fa5
Paulo <pvaz@bionexo.com>
2022-05-17 12:39:11 -0300

    feat: Pattern Observer

---
+++ src/behavioural/observer/observer.js
@@ -0,0 +1,59 @@
+var InputObservable = /** @class */ (function () {
+    function InputObservable(element) {
+        this.element = element;
+        this.observers = [];
+    }
+    InputObservable.prototype.subscribe = function () {
+        var _this = this;
+        var observer = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            observer[_i] = arguments[_i];
+        }
+        observer.forEach(function (observer) {
+            if (!_this.observers.includes(observer)) {
+                _this.observers.push(observer);
+            }
+        });
+    };
+    InputObservable.prototype.unsubscribe = function (observer) {
+        var observerIndex = this.observers.indexOf(observer);
+        if (observerIndex !== -1) {
+            this.observers.splice(observerIndex, 1);
+        }
+    };
+    InputObservable.prototype.notify = function () {
+        var _this = this;
+        this.observers.forEach(function (observer) { return observer.update(_this); });
+    };
+    return InputObservable;
+}());
+var ParagraphObserver = /** @class */ (function () {
+    function ParagraphObserver(element) {
+        this.element = element;
+    }
+    ParagraphObserver.prototype.update = function (observable) {
+        if (observable instanceof InputObservable) {
+            this.element.innerText = observable.element.value;
+        }
+    };
+    return ParagraphObserver;
+}());
+function makeInput() {
+    var input = document.createElement('input');
+    document.body.appendChild(input);
+    return input;
+}
+function makeParagraph() {
+    var p = document.createElement('p');
+    document.body.appendChild(p);
+    p.innerText = 'Texto inicial';
+    return p;
+}
+var input = new InputObservable(makeInput());
+var p1 = new ParagraphObserver(makeParagraph());
+var p2 = new ParagraphObserver(makeParagraph());
+input.subscribe(p1, p2);
+input.element.addEventListener('keyup', function () {
+    input.notify();
+});
+input.unsubscribe(p2);
